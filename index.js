e631e0a1463cc28d55480445265c71ac514a683f
John Wehr <johnwehr@gmail.com>
2017-06-06 01:09:27 -0400

    Bring base up to date with 1.0.2. Version bump.

--- index.js
+++ index.js
@@ -2,4 +2,6 @@
  * node-sqlparser: index.js
  * Create   : 2014-05-21 18:05:12
  */
-module.exports = require('./lib/ast');
+exports.AST = require('./lib/ast');
+exports.parse = require('./lib/parse').parse;
+exports.stringify = require('./lib/stringify');
